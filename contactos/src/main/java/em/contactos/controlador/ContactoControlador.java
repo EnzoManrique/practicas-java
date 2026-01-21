841b5f2772c7608854957f8d917a083a0dbe8ed9
EnzoManrique <manriqueenzo1024@gmail.com>
2026-01-21 14:42:03 -0300

    agregar,editar y eliminar

--- contactos/src/main/java/em/contactos/controlador/ContactoControlador.java
+++ contactos/src/main/java/em/contactos/controlador/ContactoControlador.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,6 +28,41 @@ public class ContactoControlador {
         contactos.forEach(contacto -> {logger.info(contacto.toString());});
         modelo.put("contactos", contactos);
         return "index"; //index.html
+    }
+
+    @GetMapping("/agregar")
+    public String mostrarAgregar(){
+        return "agregar";//agregar.html
+    }
+
+    @PostMapping("/agregar")
+    public String agregar(@ModelAttribute("contactoForma") Contacto contacto){
+        logger.info("Agregando contacto: " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable(value="id") int idContacto, ModelMap modelo) {
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Mostrando contacto a editar: " + contacto);
+        modelo.put("contacto", contacto);
+        return "editar";
+    }
+
+    @PostMapping("/editar")
+    public String editar(@ModelAttribute("contacto") Contacto contacto){
+        logger.info("Editando contacto: " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value="id") int idContacto){
+        Contacto contacto = new Contacto();
+        contacto.setIdContacto(idContacto);
+        contactoServicio.eliminarContacto(contacto);
+        return "redirect:/";
     }
 
 }
